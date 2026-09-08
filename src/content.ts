552389e0271aeb451f9b1f88760ef0716a5567ad
Javad Sheikh <javad2n@yahoo.com>
2026-09-08 16:19:13 +0300

    removed sepehr

--- src/content.ts
+++ src/content.ts
@@ -197,7 +197,6 @@ export const team = {
   members: [
     { name: "Javad Sheikh", role: "ML Scientist", email: "javad.sheikh@glacianav.com", photo: "/team/javad.jpg", focus: "50% 32%" },
     { name: "Nima Emami", role: "ML Scientist", email: "nima.emami@glacianav.com", photo: "/team/nima.jpg", focus: "50% 28%" },
-    { name: "Sepehr Seifizarei", role: "ML Scientist", email: "sepehr.seifizarei@glacianav.com", photo: "/team/sepehr.jpg", focus: "50% 30%" },
     { name: "Wilma Tiainen", role: "Business Developer", email: "wilma.tiainen@glacianav.com", photo: "/team/wilma.jpg", focus: "50% 30%" },
   ],
 };
